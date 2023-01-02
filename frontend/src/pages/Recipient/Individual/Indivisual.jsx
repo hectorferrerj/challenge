9ffa2fd5c8dccd4bab0c8550954347d7ce0e8dca
Hector Ferrer <hector.ferrerj@gmail.com>
2023-01-02 14:31:35 -0600

    refactor(Recipient): Added recipient create logic

--- frontend/src/pages/Recipient/Individual/Indivisual.jsx
+++ frontend/src/pages/Recipient/Individual/Indivisual.jsx
@@ -1,32 +1,82 @@
-import React, { useState } from "react"
+import React from "react"
+import axios from "axios"
 import "./Individual.css"
 
 import Card from "../../../components/Card/Card"
 import Title from "../../../components/Title/Title"
 import Label from "../../../components/Label/Label"
-import Input from "../../../components/Input/Input"
 import Button from "../../../components/Button/Button"
+import { useInput } from "../../../hooks/input.hook"
+import { isRequired, isEmail } from "../../../utils/validator"
 
 const Individual = () => {
-  const [formSingle, setFormSingle] = useState({
-    name: "",
-    lastName: "",
-    email: "",
-  })
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangeHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput(isRequired)
 
-  const handleChange = (e) => {
-    const field = e.target.name
-    const nextFormState = {
-      ...formSingle,
-      [field]: e.target.value,
+  const {
+    value: enteredLastName,
+    isValid: enteredLastNameIsValid,
+    hasError: lastNameInputHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastNameInput,
+  } = useInput(isRequired)
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput(isEmail)
+
+  let formIsValid = false
+
+  if (enteredNameIsValid && enteredEmailIsValid && enteredLastNameIsValid) {
+    formIsValid = true
+  }
+
+  const sendRequest = async () => {
+    const req = {
+      name: enteredName,
+      lastName: enteredLastName,
+      email: enteredEmail,
+      newsletters: [],
     }
-    setFormSingle(nextFormState)
+    await axios
+      .post("http://localhost:3008/stori/api/recipient", req)
+      .then(({data}) => {
+        alert(`El destinatario ${data?.name} ha sido creado correctamente`)
+      })
+      .catch((e) => {
+        alert(
+          "Email o Contraseña incorrecta verifique información e intente de nuevo"
+        )
+      })
+  }
+
+  const resetValues = () => {
+    resetNameInput()
+    resetLastNameInput()
+    resetEmailInput()
   }
 
   const sendRecipient = (event) => {
     event.preventDefault()
-    console.log("Req send")
+    sendRequest()
+    resetValues()
   }
+
+  const nameClasses = nameInputHasError ? "input input-error" : "input"
+  const lastNameClasses = lastNameInputHasError ? "input input-error" : "input"
+  const emailClasses = emailInputHasError ? "input input-error" : "input"
 
   return (
     <div className="individual-container">
@@ -34,39 +84,50 @@ const Individual = () => {
         <Title text="Crear destinatario" />
         <form onSubmit={sendRecipient}>
           <Label text="Nombre" />
-          <Input
-            attribute={{
-              label: "Nombre",
-              name: "name",
-              type: "text",
-              value: formSingle.name,
-              placeholder: "Ej. Hector",
-            }}
-            handleChange={handleChange}
-          ></Input>
+          <input
+            className={nameClasses}
+            arial-label="Nombre"
+            name="name"
+            type="text"
+            placeholder="Ej. Hector"
+            value={enteredName}
+            onChange={nameChangeHandler}
+            onBlur={nameBlurHandler}
+          />
+          {nameInputHasError && (
+            <p className="error-text">Este campo es requerido</p>
+          )}
           <Label text="Apellido" />
-          <Input
-            attribute={{
-              label: "Apellido",
-              name: "lastName",
-              type: "text",
-              value: formSingle.lastName,
-              placeholder: "Ej. Ferrer",
-            }}
-            handleChange={handleChange}
-          ></Input>
+          <input
+            className={lastNameClasses}
+            arial-label="Apellido"
+            name="lastName"
+            type="text"
+            value={enteredLastName}
+            placeholder="Ej. Ferrer"
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          />
+          {lastNameInputHasError && (
+            <p className="error-text">Este campo es requerido</p>
+          )}
           <Label text="Email" />
-          <Input
-            attribute={{
-              label: "Email",
-              name: "email",
-              type: "text",
-              value: formSingle.email,
-              placeholder: "Ej. hector@mail.com",
-            }}
-            handleChange={handleChange}
-          ></Input>
-          <Button onClick={sendRecipient}>{"Crear"}</Button>
+          <input
+            className={emailClasses}
+            arial-label="Email"
+            name="email"
+            type="text"
+            value={enteredEmail}
+            placeholder="Ej. hector@mail.com"
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler}
+          />
+          {emailInputHasError && (
+            <p className="error-text">Ingrese un email valido</p>
+          )}
+          <Button isDisabled={!formIsValid} onClick={sendRecipient}>
+            {"Crear"}
+          </Button>
         </form>
       </Card>
     </div>
--- frontend/src/pages/Recipient/Massive/Massive.jsx
+++ frontend/src/pages/Recipient/Massive/Massive.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import axios from "axios"
 import "./Massive.css"
 import Card from "../../../components/Card/Card"
 import Label from "../../../components/Label/Label"
@@ -8,7 +9,57 @@ import DataTable from "react-data-table-component"
 
 const Massive = () => {
   const [file, setFile] = useState()
+  const fileReader = new FileReader();
+
   const [rowData, setRowData] = useState([])
+
+
+  const csvFileToArray = string => {
+    const headers = string.slice(0, string.indexOf("\n")).trim().split(",");
+    const rows = string.slice(string.indexOf("\n") + 1).trim().split("\r\n");
+    const rowsFiltered = rows.filter((element) => element !== '');
+
+    const rowsData = rowsFiltered.map(row => {
+      const values = row.split(",");
+      const rowDataObj = headers.reduce((element, header, index) => {
+        element[header] = values[index];
+        return element;
+      }, {});
+      return rowDataObj;
+    });
+    setRowData(rowsData);
+  };
+
+  const loadFileHandler = (event) => {
+    setFile(event.target.files[0]);
+  }
+
+  const submitFileHandler = () => {
+    if (file) {
+      fileReader.onload = (event) => {
+        csvFileToArray(event.target.result);
+      }
+
+      fileReader.readAsText(file);
+    }
+  }
+
+  const sendDataFile = async (event) => {
+    event.preventDefault()
+    const req = { recipients: rowData}
+    await axios
+      .post("http://localhost:3008/stori/api/recipients", req)
+      .then(({data}) => {
+        alert(`Los ${data.length} destinatarios han sido creados correctamente`)
+        setFile(undefined)
+        setRowData([])
+        })
+      .catch((e) => {
+        alert(
+          "Email o Contraseña incorrecta verifique información e intente de nuevo"
+        )
+      })
+  }
 
   const headers = [
     ["Nombre", "name"],
@@ -26,21 +77,6 @@ const Massive = () => {
   const imageReference =
     "https://firebasestorage.googleapis.com/v0/b/stori-newsletter.appspot.com/o/examples%2Fexample%20de%20CSV.png?alt=media&token=79945e53-9035-44db-9e9a-15592297fc04"
 
-  const handleLoadFile = (e) => {
-    console.log("Load", e)
-    let recipientsToSave = [
-      {
-        email: "hector.ferrerj@outlook.com",
-        lastName: "Ferrer",
-        name: "Hector",
-      },
-    ]
-    setRowData(recipientsToSave)
-  }
-
-  const handleSubmitFile = (e) => {
-    console.log("Submit", e)
-  }
 
   return (
     <div className="massive-container">
@@ -58,13 +94,13 @@ const Massive = () => {
               type={"file"}
               id={"csvFileInput"}
               accept={".csv"}
+              onChange={loadFileHandler}
             />
           </form>
-
           <Button
             type="submit"
-            disabled={!file}
-            onClick={handleLoadFile}
+            isDisabled={!file}
+            onClick={submitFileHandler}
           >
             Procesar archivo
           </Button>
@@ -79,7 +115,7 @@ const Massive = () => {
                 pagination
               />
 
-              <Button type="submit" disabled={false} onClick={handleSubmitFile}>
+              <Button type="submit" isDisabled={false} onClick={sendDataFile}>
                 Cargar
               </Button>
             </React.Fragment>
